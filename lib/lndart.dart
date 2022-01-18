8c634b0a9a8222b721f1b25c08fac60811248f3d
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-01-18 23:23:39 +0100

    package: init project
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

---
+++ lib/lndart.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library lndart;
+
+export 'src/lndart_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
